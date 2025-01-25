04efb02493d076ef0adb0cf25cea7b6fe405553d
Selvam2004 <selvam032004@gmail.com>
2025-01-26 04:42:36 +0530

    progress bar

--- components/profile/ProgressBar.js
+++ components/profile/ProgressBar.js
@@ -1,31 +1,30 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import * as Progress from 'react-native-progress';  
-const ProgressBarCard = ({ progress }) => {  
+
+const  ProgressBar = ({ progress }) => {
+  const progressPercentage = `${progress}%`;
 
   return (
-    <View style={styles.card}>
+    <View style={styles.container}>
       <Text style={styles.title}>Profile Completion</Text>
-      <Progress.Bar
-        progress={progress / 100}
-        width={null}
-        height={12}
-        color={progress >= 70 ? '#4CAF50' : progress>=10?'#FFC107':'#ff4d4d'}  
-        unfilledColor="#ddd"
-        borderRadius={8}
-        borderWidth={0}
-      />
-      <Text style={styles.percentage}>{progress}% Complete</Text>
+      <View style={styles.barContainer}>
+        <View
+          style={[
+            styles.progressBar,
+            { width: progressPercentage, backgroundColor:progress>=90?'green': progress >= 70 ? 'blue' : progress>=40?'#FFC107':'red' },
+          ]}
+        />
+      </View>
+      <Text style={styles.percentage}>{progressPercentage} Complete</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  card: {
-    backgroundColor: '#fff',
+  container: {
     padding: 16,
     borderRadius: 12,
-    elevation: 4, 
+    backgroundColor: '#fff',
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.2,
@@ -38,8 +37,18 @@ const styles = StyleSheet.create({
     marginBottom: 8,
     color: '#333',
   },
+  barContainer: {
+    height: 12,
+    backgroundColor: '#ddd',
+    borderRadius: 8,
+    overflow: 'hidden',
+    marginVertical: 8,
+  },
+  progressBar: {
+    height: '100%',
+    borderRadius: 8,
+  },
   percentage: {
-    marginTop: 8,
     fontSize: 16,
     fontWeight: '500',
     textAlign: 'right',
@@ -47,4 +56,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default ProgressBarCard;
+export default  ProgressBar;
